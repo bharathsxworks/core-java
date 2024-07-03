f2e74a9271fbf90c979cc7d032c3498a1ff60bed
bharathsxworkz <bharathsxworkz@gmail.com>
2024-07-02 21:52:06 -0700

    basic java working on it

---
+++ TV.java
@@ -0,0 +1,13 @@
+class TV {
+    public static void main(String[] LG) {
+        String brandName = "LG";
+        int screenSize = 55;
+        String resolution = "4K UHD";
+        int hdmiPorts = 3;
+
+        System.out.println("The brand name is " + brandName);
+        System.out.println("The screen size is " + screenSize + " inches");
+        System.out.println("The resolution is " + resolution);
+        System.out.println("The number of HDMI ports is " + hdmiPorts);
+    }
+}
